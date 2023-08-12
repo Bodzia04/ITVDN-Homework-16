9a1b92b42dc9990e6475ed1b9a8ebb85618993d4
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2023-08-12 16:19:20 +0200

    Task 2

--- file.js
+++ file.js
@@ -6,29 +6,53 @@
            В момент, когда фокус получает поле ввода password - подсказку span#passwordHint
         */
 
-let form = document.forms[0];
-let loginHint = document.querySelector('#loginHint');
-let passwordHint = document.querySelector('#passwordHint')
+// let form = document.forms[0];
+// let loginHint = document.querySelector('#loginHint');
+// let passwordHint = document.querySelector('#passwordHint')
 
-form.addEventListener('submit', function(e){
-    let loginUser = form.loginInput.value;
-    let passwordUser = form.passwordInput.value;
-    alert('Login: ' + loginUser + "\n" +  'Password: ' + passwordUser);
-});
-// Login
-form.loginInput.addEventListener('focus', function(){
-    loginHint.style.display = 'inline-block';
-});
+// form.addEventListener('submit', function(e){
+//     let loginUser = form.loginInput.value;
+//     let passwordUser = form.passwordInput.value;
+//     alert('Login: ' + loginUser + "\n" +  'Password: ' + passwordUser);
+// });
+// // Login
+// form.loginInput.addEventListener('focus', function(){
+//     loginHint.style.display = 'inline-block';
+// });
 
-form.loginInput.addEventListener('blur', function(){
-    loginHint.style.display = 'none';
-});
-//Password
-form.passwordInput.addEventListener('focus', function(){
-    passwordHint.style.display = 'inline-block';
-});
+// form.loginInput.addEventListener('blur', function(){
+//     loginHint.style.display = 'none';
+// });
+// //Password
+// form.passwordInput.addEventListener('focus', function(){
+//     passwordHint.style.display = 'inline-block';
+// });
 
-form.passwordInput.addEventListener('blur', function(){
-    passwordHint.style.display = 'none';
-});
+// form.passwordInput.addEventListener('blur', function(){
+//     passwordHint.style.display = 'none';
+// });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*
+        Задание 2:
+        Добавьте валидацию формы:
+        Поле ввода логина обязательное и должно содержать не менее 4 символов
+        Поле ввода пароля должно содержать строковое значение, состоящие не менее чем из 6 символов букв в нижнем и верхнем 
+        регистре, цифры и спец символа.
+
+        Проверку нужно организовать с помощью встроенной валидации.
+        Регулярное выражение для проверки пароля - (?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*]).{6,}
+        */
+        let form = document.forms[0];
+
+        form.addEventListener('submit', function(e){
+            let loginUser = form.loginInput.value;
+            let passwordUser = form.passwordInput.value;
+            alert('Login: ' + loginUser + "\n" +  'Password: ' + passwordUser);
+        });
+        
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+
     
